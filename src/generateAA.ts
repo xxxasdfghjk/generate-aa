c2e405834e6d332dd2fdaebd91139b343bb028ba
xxxasdfghjk <xxxasdfghjk999@gmail.com>
2024-02-27 19:50:29 +0900

    横幅がいくらでも生成できるよう変更

--- src/generateAA.ts
+++ src/generateAA.ts
@@ -1,53 +1,50 @@
 import { Image } from "image-js";
 // ***default ascii Set "WM@QBGNROD&SE$Hm8Kg6ZU9%CwXAP0qpb3d52#VFaeh4koyYsTunzxJcL7>]<[?v{=}f1+j)(tIl^r|!i/~\"*;_-:',`. ";
 const COLOR_SET = "WM@QBGNROD&SEHm8TunzxJ><?=f1+t^r|/~\";_-:',`. ";
-const DEFAULT_WEIGHTED_MATRIX_MAP = [1, 0.25, 0.075, 0.005];
-const DEFALUT_WEIGHT = 0.0005;
 
 const pixelToChar = (num: number) => {
     return COLOR_SET[Math.floor((num / 255) * COLOR_SET.length) - 1] ?? COLOR_SET[0];
 };
+type ResultType = {
+    resultString: string;
+    originImageWidth: number;
+    originImageHeight: number;
+};
 
-const generateAA = async (file: string | ArrayBuffer | Uint8Array, maxWidth: number) => {
+const generateAA = async (file: string | ArrayBuffer | Uint8Array, maxWidth: number): Promise<ResultType> => {
     if (maxWidth <= 0) {
         throw new Error("invalid maxWidth parameter.");
     }
     let resultString = "";
     const sharpStream = await Image.load(file);
+
     const { width, height } = { width: sharpStream.width, height: sharpStream.height };
-
-    const pixels = sharpStream.grey().getRGBAData();
-    //@ts-ignore
-    const minPixelValue = pixels.reduce((prev, cur) => Math.min(prev, cur), 100000000);
-    //@ts-ignore
-    const maxPixelValue = pixels.reduce((prev: number, cur: number) => Math.max(prev, cur), 0);
-
-    const pixelArray = sharpStream
-        .grey()
-        .getRGBAData()
-        .filter((_, i) => i % 4 === 0)
-        .map((pixel) => ((pixel - minPixelValue) / (maxPixelValue - minPixelValue)) * 255);
-
-    const scale = Math.ceil(width / maxWidth);
-    for (let i = 0; i < Math.floor(height / (scale * 2)); i++) {
-        for (let j = 0; j < Math.floor(width / scale); j++) {
-            let sum = 0;
-            let count = 0;
-            for (let k = 0; k < scale * 2; k++) {
-                for (let l = 0; l < scale; l++) {
-                    const index = Math.floor(j * scale + (2 * scale * i + k - scale) * width + l - scale / 2);
-                    const weight =
-                        DEFAULT_WEIGHTED_MATRIX_MAP[Math.abs(k - scale) + Math.abs(Math.round((2 * l - scale) / 2))] ??
-                        DEFALUT_WEIGHT;
-                    if (pixelArray[index] !== undefined) count += weight;
-                    sum += pixelArray[index] ? pixelArray[index] * weight : 0;
-                }
-            }
-            resultString += pixelToChar(sum / count);
+    const scale = maxWidth / width;
+    const gray = sharpStream.resize({ factor: scale }).grey({ mergeAlpha: true });
+    let minPixelValue = 999999999999;
+    let maxPixelValue = -99999999999;
+    const rawData = gray.getRGBAData();
+    const pixelArray = [];
+    // グレーは1チャンネルの値のみあれば十分
+    for (let i = 0; i < rawData.length; i += 4) {
+        maxPixelValue = Math.max(maxPixelValue, rawData[i]);
+        minPixelValue = Math.min(minPixelValue, rawData[i]);
+        pixelArray.push(rawData[i]);
+    }
+    for (let i = 0; i < gray.height / 2; i++) {
+        for (let j = 0; j < gray.width; j++) {
+            const a1 = pixelArray[j + 2 * i * gray.width];
+            const a2 = pixelArray[j + (2 * i + 1) * gray.width];
+            // コントラストストレッチ
+            resultString += pixelToChar(
+                ((a1 - minPixelValue) / (maxPixelValue - minPixelValue) +
+                    (a2 - minPixelValue) / (maxPixelValue - minPixelValue)) *
+                    127.5
+            );
         }
         resultString += "\n";
     }
-    return resultString;
+    return { resultString, originImageWidth: width, originImageHeight: height };
 };
 
 export default generateAA;
--- test/generateAATest.ts
+++ test/generateAATest.ts
@@ -1,10 +1,10 @@
 import generateAA from "../src/generateAA";
 import path from "path";
 const SAMPLE_IMAGE_NAME = "01.png";
-const tryMaxWidths = [1, 2, 4, 8, 16, 32, 64, 128, 200, 256];
+const tryMaxWidths = [1, 2, 4, 8, 16, 32, 64, 128, 200, 201, 202, 203, 204];
 tryMaxWidths.forEach((width) => {
-    generateAA(path.join(__dirname, `./../sample/${SAMPLE_IMAGE_NAME}`), width).then((e) => {
-        console.log("\u001b[30m" + "\u001b[47m" + e + "\u001b[0m");
+    generateAA(path.join(__dirname, `./../sample/${SAMPLE_IMAGE_NAME}`), width).then(({ resultString }) => {
+        console.log("\u001b[30m" + "\u001b[47m" + resultString + "\u001b[0m");
         console.log("====");
     });
 });
